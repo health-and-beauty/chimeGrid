5a3a5c937ad70703453836ffc479e29c0c16d6e0
Eriks Vitolins <evitolins@gmail.com>
2016-04-12 23:12:42 -0700

    ocsillators are now dymanically created

--- js/main.js
+++ js/main.js
@@ -1,29 +1,43 @@
 (function(){ 'use strict';
 
-
+///////////////////////////////////////////////////////////////////////////////
 // Utils
+///////////////////////////////////////////////////////////////////////////////
 var remapRange = function (val, origRange, newRange) {
 	return ((newRange[1]-newRange[0]) * (val - origRange[0])) / (origRange[1] - origRange[0]);
 };
 
-// Audio
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Shims 
+///////////////////////////////////////////////////////////////////////////////
 window.AudioContext = (function(){
   return  window.AudioContext ||
           window.webkitAudioContext;
 })();
 
+navigator.getUserMedia  = navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Audio
+///////////////////////////////////////////////////////////////////////////////
 var actx = new AudioContext();
 var freqs = [110, 220, 440, 880, 1760];
-var notes = [];
-for (var i = 0; i < freqs.length; i++) {
-	notes.push(new Oscillator(actx, freqs[i], 'sine'));
-}
+var pulses = [];
 
-
+///////////////////////////////////////////////////////////////////////////////
 // Video
+///////////////////////////////////////////////////////////////////////////////
 var elem = {
 	canvas : document.getElementById('mainCanvas'),
-	media : document.getElementById('mainVideo')
+	media : document.createElement('video'),
+	playBtn : document.getElementById('playToggle')
 };
 var vctx = elem.canvas.getContext('2d');
 
@@ -32,37 +46,59 @@ var getCoordBrightness = function (context, x, y) {
 	return (imgData.data[0] + imgData.data[1] + imgData.data[2]) / 3;
 };
 
+
 var getNoteFromBrightness = function (brightness) {
 	var rangeColor = [0, 255];
 	var rangeNotes = [0, (freqs.length - 1)];
 	var note = Math.round(remapRange(brightness, rangeColor, rangeNotes));
 	return note;
 };
+var getBrightnessFromNote = function (note) {
+	var rangeColor = [0, 255];
+	var rangeNotes = [0, (freqs.length - 1)];
+	var brightness = Math.round(remapRange(note, rangeNotes, rangeColor));
+	return brightness;
+};
 
-// var renderPulse = function (note) {
-// 	if (typeof(note) === 'number' && note >= 0) {
-// 		var xoffset = note * 100;
-// 		vctx.save();
-// 		vctx.rect(xoffset, 20, 50, 50);
-// 		vctx.fill();
-// 		vctx.restore();
-// 	}
-// };
-
-var square = function (w, h) {
+var square = function (w, h, brightness) {
 	var canvas = document.createElement('canvas');
 	var ctx = canvas.getContext('2d');
 	canvas.width = w;
 	canvas.height = h;
+	ctx.fillStyle = 'rgb('+brightness+','+brightness+','+brightness+')';
 	ctx.rect(0, 0, w, h);
 	ctx.fill();
 	return canvas;
 };
 
+var crosshair = function (w, h) {
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	canvas.width = w;
+	canvas.height = h;
+
+	ctx.save();
+	ctx.fillStyle = 'red';
+	ctx.translate(canvas.width/2, 0);
+	ctx.rect(0, 0, 1, w);
+	ctx.fill();
+	ctx.restore();
+
+	ctx.save();
+	ctx.fillStyle = 'red';
+	ctx.translate(0, canvas.height/2);
+	ctx.rect(0, 0, h, 1);
+	ctx.fill();
+	ctx.restore();
+	return canvas;
+};
+
+
 var renderPulse = function (note) {
 	if (typeof(note) === 'number' && note >= 0) {
 		var xoffset = note * 100;
-		vctx.drawImage(square(50,50), xoffset, 20);
+		var brightness = getBrightnessFromNote(note);
+		vctx.drawImage(square(50, 50, brightness), xoffset, 20);
 	}
 };
 
@@ -71,10 +107,11 @@ var sampleGrid = function (x, y) {
 	var brightness = getCoordBrightness(vctx, x, y);
 	var note = getNoteFromBrightness(brightness);
 	var o = new Oscillator(actx, freqs[note], 'triangle');
-	o.pulse(0, 500);
-	//notes[note].pulse(0, 10);
+	o.pulse(0, 100);
 	renderPulse(note);
+	vctx.drawImage(crosshair(50, 50), x-25, y-25);
 };
+
 
 var mediaCanPlay = function () {
 	if (!elem.media.paused) {
@@ -108,7 +145,7 @@ var update = function () {
 };
 
 elem.media.oncanplay = mediaCanPlay;
-var refresher = new Refresher(update, 1000);
+var refresher = new Refresher(update, 10);
 refresher.start();
 
 
@@ -116,13 +153,9 @@ refresher.start();
 
 
 
-
+///////////////////////////////////////////////////////////////////////////////
 // Webcam
-navigator.getUserMedia  = navigator.getUserMedia ||
-                          navigator.webkitGetUserMedia ||
-                          navigator.mozGetUserMedia ||
-                          navigator.msGetUserMedia;
-
+///////////////////////////////////////////////////////////////////////////////
 var hdConstraints = {
   video: {
     mandatory: {
@@ -153,6 +186,6 @@ if (navigator.getUserMedia) {
   elem.media.src = 'somevideo.webm'; // fallback.
 }
 
-
-
+elem.playBtn.addEventListener('click', function(){if (!elem.media.paused) {elem.media.pause()} else {elem.media.play();}});
+window.elem = elem;
 })();
