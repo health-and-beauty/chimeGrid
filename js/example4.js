ee85fe7d47632d1f127ef9a20c1e0656a985ec09
Eriks Vitolins <eriks.vitolins@hackandpaint.com>
2018-02-10 10:38:38 -0800

    Refactor and fix video mirroring

--- js/example4.js
+++ js/example4.js
@@ -21,76 +21,8 @@ navigator.getUserMedia = (function(){
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// Audio
+// MIDI
 ///////////////////////////////////////////////////////////////////////////////
-var pulseLength = 100;
-var refreshRate = 100;
-var THRESHHOLD = 20;
-
-// [220, 440, 880, 1760]  [177, 179, 188, 190]  [301, 308, 327, 580]
-// var triggers = [
-//     {point: [220, 200], freq: 220, brightness: undefined, active: false},
-//     {point: [220, 300], freq: 330, brightness: undefined, active: false},
-//     {point: [220, 400], freq: 440, brightness: undefined, active: false},
-//     {point: [420, 400], freq: 880, brightness: undefined, active: false},
-//     {point: [420, 200], freq: 1760, brightness: undefined, active: false},
-// ];
-
-// var triggers = [
-//     {point: [100, 100], freq: getFrequency('A3'), brightness: undefined, active: false},
-//     {point: [150, 100], freq: getFrequency('B3'), brightness: undefined, active: false},
-//     {point: [200, 100], freq: getFrequency('C4'), brightness: undefined, active: false},
-//     {point: [250, 100], freq: getFrequency('D4'), brightness: undefined, active: false},
-//     {point: [300, 100], freq: getFrequency('E4'), brightness: undefined, active: false},
-//     {point: [350, 100], freq: getFrequency('F4'), brightness: undefined, active: false},
-//     {point: [400, 100], freq: getFrequency('G4'), brightness: undefined, active: false},
-// ];
-
-// A minor scale
-// var triggers = [
-//     {point: [100, 100], freq: getFrequency('A3'), brightness: undefined, active: 0},
-//     {point: [150, 100], freq: getFrequency('C4'), brightness: undefined, active: 0},
-//     {point: [200, 100], freq: getFrequency('E4'), brightness: undefined, active: 0},
-//     {point: [250, 100], freq: getFrequency('A4'), brightness: undefined, active: 0},
-//     {point: [300, 100], freq: getFrequency('C5'), brightness: undefined, active: 0},
-//     {point: [350, 100], freq: getFrequency('E5'), brightness: undefined, active: 0},
-// ];
-
-var points = generateCircleCoords(200, 300, 300, 12);
-var points2 = generateCircleCoords(100, 300, 300, 12);
-var triggers = [
-    {point: points[0], note: 'E1', brightness: undefined, active: 0},
-    {point: points[1], note: 'A2', brightness: undefined, active: 0},
-    {point: points[2], note: 'C2', brightness: undefined, active: 0},
-    {point: points[3], note: 'E2', brightness: undefined, active: 0},
-    {point: points[4], note: 'A2', brightness: undefined, active: 0},
-    {point: points[5], note: 'C3', brightness: undefined, active: 0},
-    {point: points[6], note: 'E3', brightness: undefined, active: 0},
-    {point: points[7], note: 'A3', brightness: undefined, active: 0},
-    {point: points[8], note: 'C4', brightness: undefined, active: 0},
-    {point: points[9], note: 'E4', brightness: undefined, active: 0},
-    {point: points[10], note: 'A4', brightness: undefined, active: 0},
-    {point: points[11], note: 'C5', brightness: undefined, active: 0},
-];
-
-var elems = {
-	canvas : document.getElementById('mainCanvas'),
-	media : document.createElement('video'),
-	playBtn : document.getElementById('playToggle'),
-    inputSelectMediaInputs : document.getElementById("inputSelectMediaInputs"),
-    inputSelectMidiOutputs : document.getElementById("inputSelectMidiOutputs"),
-	inputPulseLength : document.getElementById('inputPulseLength'),
-  inputRefreshRate : document.getElementById('inputRefreshRate'),
-  inputThreshhold : document.getElementById('inputThreshhold'),
-  inputs: []
-};
-var vctx = elems.canvas.getContext('2d');
-
-
-var midi_devices_available = [];
-var midi_device_name;
-var midi_output;
-
 var webmidi_init = function () {
     WebMidi.outputs.forEach(function(output){
         midi_devices_available.push(output.name);
@@ -98,16 +30,18 @@ var webmidi_init = function () {
     update_midi_select(midi_devices_available);
 };
 
+
 var webmidi_update_output = function (midi_device_name) {
     // midi_device_name = "IAC Driver Bus 1";
     console.log("Setting MIDI output to " + "\""+midi_device_name+"\"");
     midi_output = WebMidi.getOutputByName(midi_device_name);
 };
 
+
 ///////////////////////////////////////////////////////////////////////////////
 // Video
 ///////////////////////////////////////////////////////////////////////////////
-var sampleCoord = function (trigger) {
+var refreshTriggers = function (trigger) {
     var tx = trigger.point[0];
     var ty = trigger.point[1];
     var tb = trigger.brightness;
@@ -115,22 +49,25 @@ var sampleCoord = function (trigger) {
 	var b = getCoordBrightness(vctx, tx, ty);
     var threshhold = THRESHHOLD;
     var bdiff = Math.abs(tb - b);
+    var max_velocity = 0.9;
 
     if (tb !== undefined && bdiff > threshhold && midi_output) {
         trigger.active++;
-        midi_output.playNote(tnote, "all", {duration: pulseLength, velocity: 1/trigger.active});
+        midi_output.playNote(tnote, "all", {duration: PULSELENGTH, velocity: max_velocity/trigger.active});
+        
         setTimeout(
             (function (trigger) {
-                return function () {
+                return function () { 
                     trigger.active--;
                 };
             })(trigger),
-            pulseLength
+            PULSELENGTH
         );
     }
     trigger.brightness = b;
-    var size = 30 * (trigger.active + 1);
-    var thickness = 430 / (trigger.active + 1) || 1;
+    var size = 10 * (trigger.active + 1);
+    // var thickness = 430 / (trigger.active + 1) || 1;
+    var thickness = 10;
     var opacity = 1/trigger.active;
     var color = (trigger.active) ? 'rgba(255,255,255,'+opacity+')' : 'rgba(255, 255, 255, 0.2)';
     vctx.drawImage(crosshair(size, size, color, thickness), tx-(size/2), ty-(size/2));
@@ -148,24 +85,29 @@ var togglePlayback = function () {
 
 var drawVideo = function () {
 	vctx.save();
-	vctx.translate(elems.canvas.width, 0);
-	vctx.scale(-1, 1);
+    if (FLIP_HORIZONTAL) {
+        vctx.translate(elems.canvas.width, 0);
+        vctx.scale(-1, 1);
+    } else {
+        vctx.translate(0, 0);
+        vctx.scale(1, 1);
+    }
 	vctx.drawImage(elems.media, 0, 0);
 	vctx.restore();
 };
 
 
-var update = function () {
+var update_canvas = function () {
 	if (elems.media.paused) {
 		return;
 	}
     clear(vctx);
 	drawVideo();
-	triggers.forEach(function(trigger){sampleCoord(trigger);});
+	triggers.forEach(function(trigger){refreshTriggers(trigger);});
 };
 
 
-var init = function () {
+var init_canvas = function () {
 	if (!elems.media.paused) {
 		return;
 	}
@@ -175,18 +117,12 @@ var init = function () {
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// Webcam
+// Init Webcam
 ///////////////////////////////////////////////////////////////////////////////
-var webcamConstraints = {
-  video: {
-    width: 600,
-    height: 600
-  },
-  audio : false
+
+var errorCallback = function () {
+    alert('sorry bub');
 };
-
-
-var errorCallback = function () {alert('sorry bub');};
 
 
 var init_media_input =  function (video_url) {
@@ -238,6 +174,69 @@ var update_midi_select = function (options) {
 };
 
 
+
+
+
+
+
+var PULSELENGTH = 100;
+var REFRESHRATE = 100;
+var THRESHHOLD = 20;
+var FLIP_HORIZONTAL = false;
+
+var points = generateCircleCoords(200, 300, 300, 12);
+var points2 = generateCircleCoords(100, 300, 300, 12);
+var triggers = [
+    {point: points[0], note: 'E1', brightness: undefined, active: 0},
+    {point: points[1], note: 'A2', brightness: undefined, active: 0},
+    {point: points[2], note: 'C2', brightness: undefined, active: 0},
+    {point: points[3], note: 'E2', brightness: undefined, active: 0},
+    {point: points[4], note: 'A2', brightness: undefined, active: 0},
+    {point: points[5], note: 'C3', brightness: undefined, active: 0},
+    {point: points[6], note: 'E3', brightness: undefined, active: 0},
+    {point: points[7], note: 'A3', brightness: undefined, active: 0},
+    {point: points[8], note: 'C4', brightness: undefined, active: 0},
+    {point: points[9], note: 'E4', brightness: undefined, active: 0},
+    {point: points[10], note: 'A4', brightness: undefined, active: 0},
+    {point: points[11], note: 'C5', brightness: undefined, active: 0},
+
+    {point: points2[0], note: 'E1', brightness: undefined, active: 0},
+    {point: points2[1], note: 'A2', brightness: undefined, active: 0},
+    {point: points2[2], note: 'C2', brightness: undefined, active: 0},
+    {point: points2[3], note: 'E2', brightness: undefined, active: 0},
+    {point: points2[4], note: 'A2', brightness: undefined, active: 0},
+    {point: points2[5], note: 'C3', brightness: undefined, active: 0},
+    {point: points2[6], note: 'E3', brightness: undefined, active: 0},
+    {point: points2[7], note: 'A3', brightness: undefined, active: 0},
+    {point: points2[8], note: 'C4', brightness: undefined, active: 0},
+    {point: points2[9], note: 'E4', brightness: undefined, active: 0},
+    {point: points2[10], note: 'A4', brightness: undefined, active: 0},
+    {point: points2[11], note: 'C5', brightness: undefined, active: 0},    
+];
+
+var elems = {
+    canvas : document.getElementById('mainCanvas'),
+    media : document.createElement('video'),
+    playBtn : document.getElementById('playToggle'),
+    inputSelectMediaInputs : document.getElementById("inputSelectMediaInputs"),
+    inputSelectMidiOutputs : document.getElementById("inputSelectMidiOutputs"),
+    inputPulseLength : document.getElementById('inputPulseLength'),
+    inputRefreshRate : document.getElementById('inputRefreshRate'),
+    inputThreshhold : document.getElementById('inputThreshhold'),
+    inputs: []
+};
+var vctx = elems.canvas.getContext('2d');
+var webcamConstraints = {
+  video: {
+    width: 600,
+    height: 600
+  },
+  audio : false
+};
+var midi_devices_available = [];
+var midi_device_name;
+var midi_output;
+
 // MIDI Init
 WebMidi.enable(function (err) {
     if (err) {
@@ -261,8 +260,8 @@ triggers.forEach(function(trigger){
     elems.inputs.push(elem);
 });
 
-elems.media.oncanplay = init;
-var refresher = new Refresher(update, refreshRate);
+elems.media.oncanplay = init_canvas;
+var refresher = new Refresher(update_canvas, REFRESHRATE);
 refresher.start();
 
 
@@ -272,19 +271,21 @@ elems.inputSelectMidiOutputs.addEventListener('change', function (e) {
 
 elems.inputSelectMediaInputs.addEventListener('change', function (e) {
     if (event.target.value == "webcam") {
+        FLIP_HORIZONTAL = true;
         init_media_input();
     } else if (event.target.value == "video") {
+        FLIP_HORIZONTAL = false;
         init_media_input('images/Insecure_Season_2_Official_Trailer__2017__HBO.mp4');
     }
 });
 
 elems.playBtn.addEventListener('click', togglePlayback);
 elems.inputPulseLength.addEventListener('change', function (e) {
-    pulseLength = this.value;
+    PULSELENGTH = this.value;
 });
 elems.inputRefreshRate.addEventListener('change', function (e) {
-    refreshRate = this.value;
-    refresher.setFreq(refreshRate);
+    REFRESHRATE = this.value;
+    refresher.setFreq(REFRESHRATE);
 });
 elems.inputThreshhold.addEventListener('change', function (e) {
     THRESHHOLD = this.value;
